e4a1f1f25b308b0558fdf3e21b2dc508420c9bfe
Raj Pandya <rajpandya737@gmail.com>
2023-07-15 11:27:26 -0400

    add squarify function

--- config.py
+++ config.py
@@ -1,11 +1,14 @@
 # Color of the webcam box // Black 
 BOX_COLOR = (0,0,0)
 
-#Edges of the box on the camera
+# Edges of the box on the camera
 LEFT = 50
 TOP = 50
 RIGHT = 300
 BOTTOM = 300
 
-#spacing between the lines in the box, since its a square should be symetric up & down
+# spacing between the lines in the box, since its a square should be symetric up & down
 SPACING = (RIGHT - LEFT) // 3 
+
+# width of ther border of the square
+BORDER_THICKNESS = 2
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 from config import *
 import numpy as np
 from PIL import Image
+import os
 
 def get_cube ():
     #gloablish variables
@@ -27,7 +28,7 @@ def get_cube ():
             #Clever little code snippet basically makes the whole square box 
             for row in range (3):
                 for col in range(3):
-                    cv2.rectangle(frame, (LEFT + col * SPACING, TOP + row*SPACING), (LEFT + (col+1) * SPACING, TOP + (row+1) * SPACING), BOX_COLOR, 2)
+                    cv2.rectangle(frame, (LEFT + col * SPACING, TOP + row*SPACING), (LEFT + (col+1) * SPACING, TOP + (row+1) * SPACING), BOX_COLOR, BORDER_THICKNESS)
  
 
             # Display 
@@ -55,14 +56,50 @@ def get_cube ():
 def convert_to_img ():
     for n in range(1, 7):
         image = Image.open(f"sides/side{n}.jpg")
-
         cropped_image = image.crop((LEFT, TOP, RIGHT, BOTTOM))
         cropped_image.save(f"cropped_sides/cside{n}.jpg")
+        delete_img (f"sides/side{n}.jpg")
+
+def delete_img (path):
+    if os.path.exists(path):
+        os.remove(path)
+    else:
+        print("Not Found")
+
+
+def squarify(n):
+    image = Image.open(f"cropped_sides/cside{n}.jpg")
+
+    # Turn each images sides into 9 squares with their own color
+    for i in range(3):
+        for j in range(3):    
+            # Coordinates for cropping 
+            left = j * SPACING
+            top = i * SPACING
+            right = left + SPACING
+            bottom = top + SPACING
+                
+            cropped_image = image.crop((left, top, right, bottom))
+                
+            # Save the cropped image
+            chunk_number = i * 3 + j + 1
+            cropped_image.save(f"squares/cside{chunk_number}.jpg")
+
+
+    
+def avg_color (path):
+    image = Image.open(path)
+    resized_image = image.resize((1, 1))
+    rgb = resized_image.getpixel((0, 0))
+    return rgb
 
 
 def main():
     get_cube() # method to get the cubes photos and screenshots
     convert_to_img()
+    squarify (1)
+    for n in range(1, 9):
+        print(avg_color(f"squares/cside{n}.jpg"))
 
 
 
