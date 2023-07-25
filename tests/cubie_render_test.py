02275d91ddba62979f0a5b2cc7e90fc63b0876d4
Raj Pandya <rajpandya737@gmail.com>
2023-07-25 13:04:00 -0400

    add colors on each face of each cubie

--- tests/cubie_render_test.py
+++ tests/cubie_render_test.py
@@ -1,24 +1,64 @@
+ 
+
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import numpy as np
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
-def cubes(sides):
-    data = np.ones(sides, dtype=bool)
-    alpha = 1
-    colors = np.empty(sides + [4], dtype=float)
-    colors[0] = [1, 0, 0, alpha]
-    colors[1] = [1, 0, 0, alpha]
-    colors[2] = [1, 0, 0, alpha]
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
 
-    fig = plt.figure(figsize=(9, 9))
-    ax = fig.add_subplot(111, projection='3d')
-    ax.voxels(data, facecolors=colors, edgecolors='white')
-    plt.title("Demonstrating three-dimensional cubes")
-    plt.show()
+# Define the vertices of the cube
+vertices = [
+    [0, 0, 0],
+    [1, 0, 0],
+    [1, 1, 0],
+    [0, 1, 0],
+    [0, 0, 1],
+    [1, 0, 1],
+    [1, 1, 1],
+    [0, 1, 1]
+]
 
-def main():
-    sides = [3, 3, 3]  # Changed to create a 3x3x3 cube
-    cubes(sides)
+# Define the faces of the cube using the vertices
+faces = [
+    [vertices[0], vertices[1], vertices[2], vertices[3]],  # Bottom face
+    [vertices[4], vertices[5], vertices[6], vertices[7]],  # Top face
+    [vertices[0], vertices[1], vertices[5], vertices[4]],  # Front face
+    [vertices[2], vertices[3], vertices[7], vertices[6]],  # Back face
+    [vertices[0], vertices[3], vertices[7], vertices[4]],  # Left face
+    [vertices[1], vertices[2], vertices[6], vertices[5]]   # Right face
+]
 
-if __name__ == "__main__":
-    main()
+# Define colors for each face
+colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange']
+
+# Create the Poly3DCollection and add it to the plot
+cube = Poly3DCollection(faces, facecolors=colors, linewidths=1, edgecolors='r', alpha=0.99)
+cube2 = Poly3DCollection(faces, facecolors=colors, linewidths=1, edgecolors='r', alpha=0.99)
+ax.add_collection3d(cube)
+ax.add_collection3d(cube2)
+
+# Set the axis limits
+ax.set_xlim([-2, 2])
+ax.set_ylim([-2, 2])
+ax.set_zlim([-2, 2])
+
+# Add labels for clarity (optional)
+ax.set_xlabel('X-axis')
+ax.set_ylabel('Y-axis')
+ax.set_zlabel('Z-axis')
+
+shift_x, shift_y, shift_z = 0, 0, 0
+for vertex in vertices:
+    vertex[0] += shift_x
+    vertex[1] += shift_y
+    vertex[2] += shift_z
+cube.set_verts(faces)
+
+shift_x, shift_y, shift_z = 1, 1, 0
+for vertex in vertices:
+    vertex[0] += shift_x
+    vertex[1] += shift_y
+    vertex[2] += shift_z
+cube2.set_verts(faces)
+
+plt.show()
